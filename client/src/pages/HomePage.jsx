427a88cf379b19eb1c74996c4df0c688477a8866
ZeroUnoDLC <102610799+ZeroUnoDLC@users.noreply.github.com>
2023-12-16 17:03:16 -0500

    HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,23 @@
+import '../styles/HomePage.css'
+import logo from '../assets/logo_UTN.png' 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+      <body>
+        <div className='contenido'>
+          <img src={logo} alt="Logo_Universidad" />
+          <h1>Universidad Técnica del Norte</h1>
+          <h2>Seguridad informatica</h2>
+          <h3>Seguridad implementada a un aplicativo de venta de productos (CRUD)</h3>
+        </div>
+        <div className='integrantes'>
+          <h4>de la Cruz Espinosa Brayan</h4>
+          <h4>Echeverría Soto Victor Eduardo</h4>
+          <h4>Rosero Cuaspud Jorge Rafael</h4>
+          <h4>Sandoval Pumisacho Alan Davor</h4>
+        </div>
+      </body>
+    </>
   )
 }
 
